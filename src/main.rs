53526bdd6ea5572238a6a29a60da87f420482bc8
destumme <destumme@gmail.com>
2025-10-16 07:50:07 -0500

    healthcheck

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+/**
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World!");
+    format!("Hello {}", &name)
+}
+**/
+
+async fn health_check(_req: HttpRequest) -> impl Responder {
+    HttpResponse::Ok()
+}
+
+//#[tokio::main]
+fn main() -> Result<(), std::io::Error> {
+    let startup = async move {HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+    })
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
+    };
+
+    //same as making main async w/ #[tokio::main]
+    tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()?
+        .block_on(startup)
 }
