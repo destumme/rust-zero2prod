763e74a132b14516d22bea4c30223aa598537130
destumme <destumme@gmail.com>
2025-10-17 07:43:23 -0500

    adding test

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,15 @@
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
+use zero2prod::run;
 
 //#[tokio::main]
 fn main() -> Result<(), std::io::Error> {
-    let startup = async move {HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-    })
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+    let body = async move {
+        run()?.await
     };
 
     //same as making main async w/ #[tokio::main]
     tokio::runtime::Builder::new_current_thread()
         .enable_all()
         .build()?
-        .block_on(startup)
+        .block_on(body)
 }
