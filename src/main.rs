a95068fd7d65855e51a1062a37aa9b144b9e9bbb
destumme <destumme@gmail.com>
2025-10-22 07:28:18 -0500

    chpt 3.5

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
+use std::net::TcpListener;
 use zero2prod::run;
 
 //#[tokio::main]
 fn main() -> Result<(), std::io::Error> {
-    let body = async move { run()?.await };
+    let listener =
+        TcpListener::bind("127.0.0.1:0")
+            .expect("failed to bind to random port");
+    let port = listener.local_addr().unwrap().port();
+    println!("Server will start on port:{}", port);
 
     //same as making main async w/ #[tokio::main]
+    let body =
+        async move { run(listener)?.await };
     tokio::runtime::Builder::new_current_thread()
         .enable_all()
         .build()?
