581df2c8233008877ee4f56d2f7501b68ea09c32
destumme <destumme@gmail.com>
2025-10-22 07:31:46 -0500

    line length 80

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,17 @@
-use std::net::TcpListener;
 use actix_web::dev::Server;
 use actix_web::{App, HttpResponse, HttpServer, Responder, web};
+use std::net::TcpListener;
 
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(||
-            App::new().route(
-                "/health_check",
-                web::get().to(health_check)
-            ))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new().route("/health_check", web::get().to(health_check))
+    })
+    .listen(listener)?
+    .run();
 
     Ok(server)
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -21,13 +21,10 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 fn spawn_app() -> String {
-    let listener =
-        TcpListener::bind("127.0.0.1:0")
-            .expect("failed to bind to random port");
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("failed to bind to random port");
     let port = listener.local_addr().unwrap().port();
-    let server =
-        zero2prod::run(listener)
-            .expect("Failed to bind address");
+    let server = zero2prod::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
     format!("http://127.0.0.1:{}", port)
 }
